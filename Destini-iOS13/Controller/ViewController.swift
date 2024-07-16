bcb8a5500ea89bd58d6db3e7746ab69bc6b89d4b
hakmace <github.sworn439@passmail.net>
2024-07-16 20:18:38 +0200

    Finish the project
    Finished the story adventure project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var userStories = StoryBrain()
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        print("button pressed")
+        let userChoice = sender.currentTitle!
+        userStories.checkChoice(userChoice)
+        updateUI()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+        
+        
+        func updateUI() {
+            storyLabel.text = userStories.getStoryText()
+            choice1Button.setTitle(userStories.getChoice1(), for: .normal)
+            choice2Button.setTitle(userStories.getChoice2(), for: .normal)
+        }
 
     }
-
-
-}
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice2: String
+    let choice1Destination: Int
+    let choice2Destination: Int
+    
+    init(t: String, c1: String, c1D: Int, c2: String, c2D: Int) {
+        title = t
+        choice1 = c1
+        choice2 = c2
+        choice1Destination = c1D
+        choice2Destination = c2D
+    }
+}
